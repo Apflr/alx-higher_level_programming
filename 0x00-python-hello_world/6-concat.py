ea2beaf802528738d69b66b2da563ec02eeb82f2
Naomi Duncan <springnaomi@gmail.com>
2023-11-27 16:45:11 -0800

    python

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str + str + str)
+print(str[:9])
+root@748c5573d9ff:/alx-higher_level_programming/0x00-python-hello_world# cat 6-concat.py
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
