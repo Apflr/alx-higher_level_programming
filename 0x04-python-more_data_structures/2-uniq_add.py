1ce364e5c8fff5ed31ad875d258a27ea1215d8df
Naomi <springnaomi6@gmail.com>
2024-03-04 09:57:40 -0800

    J

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def uniq_add(my_list=[]):
+    unique1 = []
+    for x in my_list:
+        if x not in unique1:
+            unique1.append(x)
+    total = sum(unique1)
+    return total
+
