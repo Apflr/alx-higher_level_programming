2d69ececa57369475eb0623a95ebaee5172d7886
Naomi <springnaomi6@gmail.com>
2024-03-05 04:49:09 -0800

    hi

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-safe_function = __import__('101-safe_function').safe_function
+import sys
 
 
-def my_div(a, b):
-    return a / b
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
